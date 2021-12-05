6eb8dd7381de63beb1bf72f982808a65ec4208c4
frontdevruso <frontdevruso@gmail.com>
2021-12-05 22:30:53 +0300

    dashboard-mini-chart-layout

--- src/components/Dashboard/TokenPricesCharts/token-prices-charts.js
+++ src/components/Dashboard/TokenPricesCharts/token-prices-charts.js
@@ -4,11 +4,12 @@ import arrowUp from './arrow-up.svg';
 import arrowDown from './arrow-down.svg';
 import chart from './../../../assets/charts/chart.svg'
 import demoChart from './../../../assets/charts/demo-chart.svg'
-import {LineChart, XAxis, YAxis, Line, ResponsiveContainer, Tooltip} from 'recharts';
+import {LineChart, XAxis, YAxis, Line, ResponsiveContainer, Tooltip, AreaChart, Area} from 'recharts';
 import {} from '../../App/App';
 import {useSystemContext} from '../../../systemProvider';
 import {useDashboardContext} from '../../../providers/dashboard-provider';
 import styled from 'styled-components';
+import { TVLChart } from '../TVLChart/TVLChart';
 
 
 const TokenPriceChartWrapper = styled.div`
@@ -96,9 +97,12 @@ const TokenPriceChartWrapper = styled.div`
         opacity: ${props => props.isWindowExpanded ? "1" : "0"};
         transition: ${props => props.isWindowExpanded ? "2s all" : "0.1s all"};
 
-        img {
-          width: auto;
+        .chart-wrapper {
+          width: 17.708vw;
           height: 8.490vw;
+
+          cursor: pointer;
+          font-size: 0.521vw;
         }
 
         main {
@@ -262,65 +266,60 @@ export const TokenPricesCharts = () => {
     const {dashTokens} = useDashboardContext();
 
     const CustomToolTip = ({active, payload, label}) => {
-
-
-        if (payload[0]) {
-            const value = payload[0].payload.value;
-            if (active) {
-                return (
-                    <div className='custom-tooltip'>
-                        <span> {value}$ </span>
-                    </div>
-                )
-            }
-        }
-
-        return null;
-
+      if (payload[0]) {
+          const value = payload[0].payload.value;
+          if (active) {
+              return (
+                  <div className='custom-tooltip'>
+                      <span> {value}$ </span>
+                  </div>
+              )
+          }
+      }
+      return null;
     }
 
+    const data = [
+      {time: '01', uv: 100000, date: "Jul 1, 2021"},
+      {time: '02', uv: 110000, date: "Jul 2, 2021"},
+      {time: '03', uv: 90000, date: "Jul 3, 2021"},
+      {time: '04', uv: 100000, date: "Jul 4, 2021"},
+      {time: '05', uv: 150000, date: "Jul 5, 2021"},
+      {time: '06', uv: 125000, date: "Jul 6, 2021"},
+      {time: '07', uv: 126000, date: "Jul 7, 2021"},
+      {time: '08', uv: 143000, date: "Jul 8, 2021"},
+      {time: '09', uv: 130000, date: "Jul 9, 2021"},
+      {time: '10', uv: 200000, date: "Jul 10, 2021"},
+      {time: '11', uv: 400000, date: "Jul 11, 2021"},
+      {time: '12', uv: 500000, date: "Jul 12, 2021"},
+      {time: '13', uv: 600000, date: "Jul 13, 2021"},
+      {time: '14', uv: 800000, date: "Jul 14, 2021"},
+      {time: '15', uv: 1000000, date: "Jul 15, 2021"},
+      {time: '16', uv: 1100000, date: "Jul 16, 2021"},
+      {time: '17', uv: 1100000, date: "Jul 1, 2021"},
+      {time: '18', uv: 1310000, date: "Jul 2, 2021"},
+      {time: '19', uv: 900000, date: "Jul 3, 2021"},
+      {time: '20', uv: 1000000, date: "Jul 4, 2021"},
+      {time: '21', uv: 1050000, date: "Jul 5, 2021"},
+      {time: '22', uv: 1025000, date: "Jul 6, 2021"},
+      {time: '23', uv: 1260000, date: "Jul 7, 2021"},
+      {time: '24', uv: 900000, date: "Jul 8, 2021"},
+      {time: '25', uv: 1030000, date: "Jul 9, 2021"},
+      {time: '26', uv: 200000, date: "Jul 10, 2021"},
+      {time: '27', uv: 400000, date: "Jul 11, 2021"},
+      {time: '28', uv: 500000, date: "Jul 12, 2021"},
+      {time: '29', uv: 600000, date: "Jul 13, 2021"},
+      {time: '30', uv: 800000, date: "Jul 14, 2021"},
+      {time: '31', uv: 1000000, date: "Jul 15, 2021"},
+      {time: '01', uv: 900000, date: "Jul 16, 2021"},
+  ];
 
-    const Chart = ({data, fullSize}) => {
-      const tickColor = theme === "light" ? "black" : "white"
-      return (
-          <ResponsiveContainer width={'100%'} height={"100%"}>//
-              <LineChart
-                  margin={{
-                      top: 50,
-                      bottom: 1,
-                  }}
-                  data={data}
-              >
+    const [chartValue, setChartValue] = useState({
+      time: data[data.length - 1].date,
+      value: data[data.length - 1].uv
+    })
 
-                  <Line
-                      type="basis"
-                      dataKey="value"
-                      stroke="#40BA93"
-                      strokeWidth={5}
-                      dot={false}
-                      activeDot={true}
-                  />
-                  <Tooltip
-                      content={<CustomToolTip/>}
-                  />
-                  {fullSize ?
-                      <XAxis
-                          dataKey="time"
-                          axisLine={false}
-                          tickLine={false}
-                          stroke={tickColor}
-                          minTickGap={5}
-                      />
-                      :
-                      ""
-                  }
-
-              </LineChart>
-          </ResponsiveContainer>
-        )
-    }
-
-    const [showChart, setShowChart] = useState({active: true, index: 0})
+    const [showChart, setShowChart] = useState({active: true, index: 0});
 
     return (
         <TokenPriceChartWrapper isWindowExpanded={expandWindow} onClick={() => setExpandWindow(!expandWindow)}>
@@ -349,7 +348,50 @@ export const TokenPricesCharts = () => {
                           <img src={demoChart} className='demo-chart' />
                         </SinglePriceBlock>
                         <div className="price-block-chart">
-                          <img src={chart} />
+                          <div className='chart-wrapper'>
+                            <ResponsiveContainer className='responsive-container-chart' width={"100%"} height={"100%"}>
+                                <LineChart
+                                data={data}
+                                onMouseLeave={() => setChartValue({
+                                    time: data[data.length - 1].date,
+                                    value: data[data.length - 1].uv
+                                })}
+                                >
+                                <Line
+                                    type="monotone"
+                                    dataKey="uv"
+                                    stroke="rgba(64, 186, 147, 0.05)"
+                                    strokeWidth={"1vw"}
+                                    dot={false}
+                                    activeDot={true}
+                                />
+                                <Line
+                                    type="monotone"
+                                    dataKey="uv"
+                                    stroke="#40BA93"
+                                    strokeWidth={"0.25vw"}
+                                    dot={false}
+                                    activeDot={true}
+                                />
+                                <Tooltip
+                                    contentStyle={{display: 'none'}}
+                                    formatter={(value, name, props) => {
+                                        const {payload: {date, uv}} = props;
+                                        if (chartValue.value !== uv) {
+                                            setChartValue({time: date, value: uv})
+                                        }
+                                    }}
+                                />
+                                <XAxis
+                                    dataKey="time"
+                                    axisLine={false}
+                                    tickLine={false}
+                                    tick={{fontSize: "0.7vw"}}
+                                    stroke={theme === "light" ? "black" : "white"}
+                                />
+                              </LineChart>
+                            </ResponsiveContainer>
+                          </div>
                           <main>
                             <div className="token-data">
                               <p>Supply:</p>
